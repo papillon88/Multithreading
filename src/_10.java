129a6b919780298c964a530aa6a55603a5844bb6
papillon <devopreet.ch@gmail.com>
2017-09-12 15:13:57 -0500

    modified simple echo chat

--- src/_10.java
+++ src/_10.java
@@ -4,44 +4,62 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
 
 public class _10 {
 
     public static void main(String[] args){
-        Thread t1 = new Thread(()->{
+        Thread t1 = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             try (//with
-                    Socket client = new Socket("DESKTOP-4NQL187",4443);
-                    PrintWriter out = new PrintWriter(client.getOutputStream(),true);
-                    BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                    BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
+                 Socket client = new Socket("DESKTOP-4NQL187", 4443);
+                 PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                 BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
             ) {
-                Thread.sleep(3000);
                 String line;
-                while((line=stdIn.readLine())!=null){
+                while ((line = stdIn.readLine()) != null) {
                     out.println(line);
-                    System.out.printf("from server : %s%n",in.readLine());
+                    System.out.printf("from server : %s%n", in.readLine());
                 }
             } catch (IOException e) {
                 e.printStackTrace();
-            } catch (InterruptedException e){
-                        e.printStackTrace();
             }
         });
+
         Thread t2 = new Thread(()->{
-            try(//with
-                    ServerSocket serverSocket = new ServerSocket(4443);
+            try {
+                ServerSocket serverSocket = new ServerSocket(4443);
+                while(true){
                     Socket client = serverSocket.accept();
-                    PrintWriter out = new PrintWriter(client.getOutputStream(),true);
-                    BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()))
-            ) {
-                String line;
-                while ((line=in.readLine())!=null){
-                    out.println(line);
+                    Thread handlerThread =  new Thread(()->{
+                        try {
+                            PrintWriter out = new PrintWriter(client.getOutputStream(),true);
+                            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                            String line;
+                            while ((line=in.readLine())!=null){
+                                out.println(Thread.currentThread().getName()+" "+Thread.currentThread().getId()+" : "+line);
+                            }
+                            out.close();
+                            in.close();
+                            client.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                    handlerThread.start();
+                    //handlerThread.join();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         });
+
         t1.start();
         t2.start();
 
@@ -51,5 +69,22 @@ public class _10 {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void startAsNonCoordinator() {
+        startServer();
+        startClient();
+    }
+
+    private static void startAsCoordinator() {
+
+    }
+
+    private static void startClient() {
+
+    }
+
+    private static void startServer() {
+
     }
 }
